f550edf0511f3de3384427f22321122afa27bffd
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-11-20 00:20:50 +0000

    updates to devcontainer and code to .net8

---
+++ src/setup.sh
@@ -0,0 +1,12 @@
+# set to 9 or 10
+DEBIAN_VERSION=10
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/$DEBIAN_VERSION/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-4
